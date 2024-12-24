9c2fde6e81af2820044255a16020c0fa78d5b010
GaneshP123340 <123kumarganesh@gmail.com>
2024-12-24 08:34:37 +0530

    FirstProgram has been changed for Multiple statements

---
+++ src/FirstProgramHelloWorld.java
@@ -0,0 +1,20 @@
+
+public class FirstProgramHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello Welcome to First Program");
+			
+			
+			System.out.println("Hello Welcome to First Program1");	
+			System.out.println("Hello Welcome to First Program2");	
+			
+			System.out.println("Hello Welcome to First Program5");	
+			
+			System.out.println("Hello Welcome to First Program6");	
+			System.out.println("Hello Welcome to First Program4");
+			System.out.println("Hello Welcome to First Program8");	
+
+	}
+
+}
